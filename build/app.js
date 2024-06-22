91abb44c5a5e0fa022f85425a2031a141c907b57
Tomasz <tomasz@example.com>
2024-06-22 23:18:38 +0200

    adding tests with 'reqest' package

--- build/app.js
+++ build/app.js
@@ -14,7 +14,7 @@ let rok = 1991;
 router
     .get('/hello', (ctx, next) => {
     ctx.body = `\n\n ${hello} \n\n Pierwsze wydanie kernela Linuksa miało miejsce ${dzien} ${miesiac} ${rok}  `;
-    //ctx.body = `Pierwsze wydanie kernela Linuksa miało miejsce`;
+    
 })
     .get('/goodbye', (ctx, next) => {
     ctx.body = 'Goodbye, world';
--- test/test-pages.mjs
+++ test/test-pages.mjs
@@ -4,10 +4,39 @@ import { expect as _expect } from "chai";
 let expect = _expect;
 import request from 'request';
 
-it('Main page content', function(done) {
+it('"Hello" page content', function(done) {
     request('http://localhost:8080/hello' , function(error, response, body) {
         //expect(body).to.equal( `Pierwsze wydanie kernela Linuksa miało miejsce`);
         expect(body).to.equal( `\n\n Hello everybody!!! \n\n Pierwsze wydanie kernela Linuksa miało miejsce 5 Pazdziernika 1991  ` );
+        done();
+    });
+});
+
+it('"Hello" page status', function(done) {
+    request('http://localhost:8080/hello' , function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
+
+it('"Goodbye" page content', function(done) {
+    request('http://localhost:8080/goodbye' , function(error, response, body) {
+        expect(body).to.equal('Goodbye, world' );
+        done();
+    });
+});
+
+it('"Goodbye" page status', function(done) {
+    request('http://localhost:8080/goodbye' , function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
+
+
+it('"/" page content', function(done) {
+    request('http://localhost:8080/' , function(error, response, body) {
+        expect(response.statusCode).to.equal(404);
         done();
     });
 });
